bb662ec5b4d0ffdec15d98887465bb7f57e07bc8
Chicco Moshe Serby <chiccomoshe@gmail.com>
2020-06-28 00:12:08 +0300

    cors

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const session = require('express-session');
 const bodyParser = require('body-parser');
 const alertnode = require('alert-node');
-// const cors = require('cors');
+const cors = require('cors');
 const app = express();
 
 
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({
     extended: true
 }))
 
-// app.use(cors());
+app.use(cors());
 
 const TWO_HOURS = 1000 * 60 * 60 * 2;
 
